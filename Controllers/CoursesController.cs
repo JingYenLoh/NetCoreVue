e8944fe6d7b0539fc8fbaaf53cbc49c2b04e7e4c
Loh Jing Yen <lohjingyen@gmail.com>
2017-10-16 15:42:51 +0800

    Generate initial controllers

---
+++ Controllers/CoursesController.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NetCoreVue.Data;
+using NetCoreVue.Models;
+
+namespace NetCoreVue.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Courses")]
+    public class CoursesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CoursesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Courses
+        [HttpGet]
+        public IEnumerable<Course> GetCourses()
+        {
+            return _context.Courses;
+        }
+
+        // GET: api/Courses/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCourse([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var course = await _context.Courses.SingleOrDefaultAsync(m => m.CourseId == id);
+
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(course);
+        }
+
+        // PUT: api/Courses/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCourse([FromRoute] int id, [FromBody] Course course)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != course.CourseId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(course).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CourseExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Courses
+        [HttpPost]
+        public async Task<IActionResult> PostCourse([FromBody] Course course)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Courses.Add(course);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCourse", new { id = course.CourseId }, course);
+        }
+
+        // DELETE: api/Courses/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCourse([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var course = await _context.Courses.SingleOrDefaultAsync(m => m.CourseId == id);
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            _context.Courses.Remove(course);
+            await _context.SaveChangesAsync();
+
+            return Ok(course);
+        }
+
+        private bool CourseExists(int id)
+        {
+            return _context.Courses.Any(e => e.CourseId == id);
+        }
+    }
+}
---
+++ Controllers/SessionSynopsisController.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NetCoreVue.Data;
+using NetCoreVue.Models;
+
+namespace NetCoreVue.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/SessionSynopsis")]
+    public class SessionSynopsisController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SessionSynopsisController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/SessionSynopsis
+        [HttpGet]
+        public IEnumerable<SessionSynopsis> GetSessionSynopses()
+        {
+            return _context.SessionSynopses;
+        }
+
+        // GET: api/SessionSynopsis/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetSessionSynopsis([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var sessionSynopsis = await _context.SessionSynopses.SingleOrDefaultAsync(m => m.SessionSynopsisId == id);
+
+            if (sessionSynopsis == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(sessionSynopsis);
+        }
+
+        // PUT: api/SessionSynopsis/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSessionSynopsis([FromRoute] int id, [FromBody] SessionSynopsis sessionSynopsis)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != sessionSynopsis.SessionSynopsisId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(sessionSynopsis).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SessionSynopsisExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/SessionSynopsis
+        [HttpPost]
+        public async Task<IActionResult> PostSessionSynopsis([FromBody] SessionSynopsis sessionSynopsis)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.SessionSynopses.Add(sessionSynopsis);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetSessionSynopsis", new { id = sessionSynopsis.SessionSynopsisId }, sessionSynopsis);
+        }
+
+        // DELETE: api/SessionSynopsis/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSessionSynopsis([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var sessionSynopsis = await _context.SessionSynopses.SingleOrDefaultAsync(m => m.SessionSynopsisId == id);
+            if (sessionSynopsis == null)
+            {
+                return NotFound();
+            }
+
+            _context.SessionSynopses.Remove(sessionSynopsis);
+            await _context.SaveChangesAsync();
+
+            return Ok(sessionSynopsis);
+        }
+
+        private bool SessionSynopsisExists(int id)
+        {
+            return _context.SessionSynopses.Any(e => e.SessionSynopsisId == id);
+        }
+    }
+}
