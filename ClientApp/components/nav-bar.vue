bf0a0366fd973f41392aa1051cc11cd8194bad56
Loh Jing Yen <lohjingyen@gmail.com>
2017-10-28 23:14:03 +0800

    Create SessionSynopsis and CustomerAccount links

--- ClientApp/components/nav-bar.vue
+++ ClientApp/components/nav-bar.vue
@@ -6,19 +6,37 @@
         <img src="https://sentry.io/_assets/logos/vue-wordmark-3eed5d97e4588a2d4f77f7f331635f8fb238d06c7e8044b1720216058a022e2d.svg" alt="Vue is the best" width="112" height="28" />
       </router-link>
 
-      <router-link v-for="route in routes" :key="route.path" :to="route.path" class="navbar-item">
-        <span class="icon">
-          <i :class="route.style"></i>
+      <span
+        :class="{ 'navbar': true, 'navbar-burger': true, 'is-active': isActive }"
+        @click='toggle'
+      >
+        <span></span>
+        <span></span>
+        <span></span>
         </span>
-        {{ route.display }}
-      </router-link>
 
-      <button class="button navbar-burger">
-        <span></span>
-        <span></span>
-        <span></span>
-      </button>
     </div>
+
+    <div :class="{ 'navbar-menu': true, 'is-active': isActive }">
+        <!-- Sessions -->
+        <router-link class="navbar-item" :to="'/Sessions/Index'">
+          Session
+      </router-link>
+        <router-link class="navbar-item" :to="'/Sessions/Create'">
+          Create Session
+        </router-link>
+
+        <!-- Customers -->
+        <router-link class="navbar-item" :to="'/Customers/Index'">
+          Customers
+        </router-link>
+        <router-link class="navbar-item" :to="'/Customers/Create'">
+          Create Customer
+        </router-link>
+      </div>
+
+    </div>
+
   </nav>
 </template>
 
@@ -26,9 +44,15 @@
 import { routes } from '../routes'
 
 export default {
-  data() {
+  data () {
     return {
-      routes
+      routes,
+      isActive: false
+    }
+  },
+  methods: {
+    toggle () {
+      this.isActive = !this.isActive
     }
   }
 }
