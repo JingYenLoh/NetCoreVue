ff71d2ffc02a5dd1274cc76201aee43ed3fe0f2e
Loh Jing Yen <lohjingyen@gmail.com>
2017-10-28 23:25:43 +0800

    Create routes

--- ClientApp/routes.js
+++ ClientApp/routes.js
@@ -1,13 +1,84 @@
 import HomePage from 'components/home-page'
 import CoursePage from 'components/course-page'
-import CustomerPage from 'components/customer-page'
-import SessionPage from 'components/session-page'
+
+// Customer Account
+import CustomerHome from 'components/CustomerView/index'
+import CustomerPage from 'components/CustomerView/CustomerAccountPage'
+import EditCustomer from 'components/CustomerView/EditCustomerAccount'
+import CreateCustomer from 'components/CustomerView/CreateCustomerAccount'
+
+// Session Synopsis
+import SessionHome from 'components/SessionSynopsisView/index'
+import SessionPage from 'components/SessionSynopsisView/SessionSynopsisPage'
+import EditSession from 'components/SessionSynopsisView/EditSessionSynopsis'
+import CreateSession from 'components/SessionSynopsisView/CreateSessionSynopsis'
+
 import Login from 'components/LoginView/LoginPage'
 
 export const routes = [
-  { path: '/', component: HomePage, display: 'Home', style: 'fa fa-lg fa-home' },
-  { path: '/Courses', component: CoursePage, display: 'Courses', style: 'fa fa-lg fa-graduation-cap' },
-  { path: '/Customers', component: CustomerPage, display: 'Customer', style: 'fa fa-lg fa-user' },
-  { path: '/Sessions', component: SessionPage, display: 'Sessions', style: 'fa fa-lg fa-clock-o' },
-  { path: '/Login', component: Login, display: 'Login', style: 'fa fa-lg fa-user' }
+  {
+    path: '/',
+    component: HomePage,
+    display: 'Home',
+    style: 'home'
+  },
+  {
+    path: '/Courses',
+    component: CoursePage,
+    display: 'Courses',
+    style: 'graduation-cap'
+  },
+  {
+    path: '/Customers',
+    component: CustomerHome,
+    display: 'Customers',
+    children: [
+      {
+        path: 'Index',
+        component: CustomerPage,
+        display: 'Customers'
+      },
+      {
+        path: 'Create',
+        component: CreateCustomer,
+        display: 'Create Customer Account'
+      },
+      {
+        path: 'Edit/:id',
+        name: 'editCustomer',
+        component: EditCustomer,
+        props: true
+      }
+    ]
+  },
+  {
+    path: '/Sessions',
+    component: SessionHome,
+    display: 'Sessions',
+    style: 'clock-o',
+    children: [
+      {
+        path: 'Index',
+        component: SessionPage,
+        display: 'Sessions'
+      },
+      {
+        path: 'Create',
+        component: CreateSession,
+        display: 'Create Session Synopsis'
+      },
+      {
+        path: 'Edit/:id',
+        name: 'editSynopsis',
+        component: EditSession,
+        props: true
+      }
+    ]
+  },
+  {
+    path: '/Login',
+    component: Login,
+    display: 'Login',
+    style: 'user'
+  }
 ]
