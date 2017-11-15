6482c697b2825d1d0f3daf9be1eb75dd9ce82bfc
Loh Jing Yen <lohjingyen@gmail.com>
2017-11-15 16:38:35 +0800

    Format component according to new style

--- ClientApp/components/nav-bar.vue
+++ ClientApp/components/nav-bar.vue
@@ -1,33 +1,42 @@
-<template>
-  <nav class="navbar is-primary" role="navigation" aria-label="main navigation">
+﻿<template>
+  <nav class="navbar is-primary"
+       role="navigation"
+       aria-label="main navigation">
     <div class="container">
       <div class="navbar-brand">
 
-        <router-link class="navbar-item" to="/">
-          <img src="https://vuejs.org/images/logo.png" alt="Vue is the best" width="28" height="28" />
+        <router-link class="navbar-item"
+                     to="/">
+          <img src="https://vuejs.org/images/logo.png"
+               alt="Vue is the best"
+               width="28"
+               height="28" />
         </router-link>
 
-        <span
-          class="navbar navbar-burger is-primary"
-          :class="{ 'is-active': isActive }"
-          @click='isActive = !isActive'
-        >
+        <span class="navbar navbar-burger is-primary"
+              :class="{ 'is-active': isActive }"
+              @click='isActive = !isActive'>
           <span></span>
           <span></span>
           <span></span>
         </span>
       </div>
 
-      <div class="navbar-menu" :class="{ 'is-active': isActive }">
+      <div class="navbar-menu"
+           :class="{ 'is-active': isActive }">
         <div class="navbar-start">
 
           <div class="navbar-item has-dropdown is-hoverable">
             <a class="navbar-link">Session</a>
             <div class="navbar-dropdown">
-              <router-link class="navbar-item" :to="'/Sessions/Index'" exact-active-class="is-active">
+              <router-link class="navbar-item"
+                           :to="'/Sessions/Index'"
+                           exact-active-class="is-active">
                 Manage Session
               </router-link>
-              <router-link class="navbar-item" :to="'/Sessions/Create'" exact-active-class="is-active">
+              <router-link class="navbar-item"
+                           :to="'/Sessions/Create'"
+                           exact-active-class="is-active">
                 Create Session
               </router-link>
             </div>
@@ -36,16 +45,21 @@
           <div class="navbar-item has-dropdown is-hoverable">
             <a class="navbar-link">Customer</a>
             <div class="navbar-dropdown">
-              <router-link class="navbar-item" :to="'/Customers/Index'" exact-active-class="is-active">
+              <router-link class="navbar-item"
+                           :to="'/Customers/Index'"
+                           exact-active-class="is-active">
                 Manage Customers
               </router-link>
-              <router-link class="navbar-item" :to="'/Customers/Create'" exact-active-class="is-active">
+              <router-link class="navbar-item"
+                           :to="'/Customers/Create'"
+                           exact-active-class="is-active">
                 Create Customer
               </router-link>
             </div>
           </div>
 
-          <router-link class="navbar-item" :to="'/Users/Index'">
+          <router-link class="navbar-item"
+                       :to="'/Users/Index'">
             Manage User
           </router-link>
 
