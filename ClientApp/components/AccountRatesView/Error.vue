0af23020f83fc952f28db8f9e22c05867b013bd4
Loh Jing Yen <lohjingyen@gmail.com>
2017-11-25 19:13:09 +0800

    Create simple error view for when there's no account rates

---
+++ ClientApp/components/AccountRatesView/Error.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1 class="title">No account rates</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  }  
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
