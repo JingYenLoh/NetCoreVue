5d5bef5c8293392ba8e694f18b8858e6d5d674d6
Loh Jing Yen <lohjingyen@gmail.com>
2018-01-14 15:25:13 +0800

    Format page
    How did I miss this

--- ClientApp/components/CustomerView/CustomerAccountPage.vue
+++ ClientApp/components/CustomerView/CustomerAccountPage.vue
@@ -3,75 +3,89 @@
     <div class="container">
       <h1 class="title is-spaced">Customer Accounts</h1>
 
-      <b-table
-        :data="customers"  
-        striped
-        paginated
-        :per-page="perPage"
-        default-sort-direction="asc"
-        :selected.sync="selected"
-        default-sort="accountName"
-      >
+      <b-table :data="customers"
+               striped
+               paginated
+               :per-page="perPage"
+               default-sort-direction="asc"
+               :selected.sync="selected"
+               default-sort="accountName">
         <template slot-scope="props">
 
-          <b-table-column field="accountName" label="Account Name" sortable>
+          <b-table-column field="accountName"
+                          label="Account Name"
+                          sortable>
             {{ props.row.accountName }}
           </b-table-column>
 
-          <b-table-column field="accountRates" label="# of Rates Data" numeric>
+          <b-table-column field="accountRates"
+                          label="# of Rates Data"
+                          numeric>
             {{ props.row.numAccountRates }}
           </b-table-column>
-          
-          <b-table-column field="instructorAccounts" label="# of Instructors" numeric>
+
+          <b-table-column field="instructorAccounts"
+                          label="# of Instructors"
+                          numeric>
             {{ props.row.numInstructors }}
           </b-table-column>
-          
-          <b-table-column field="updatedBy" label="Updated By" sortable>
+
+          <b-table-column field="updatedBy"
+                          label="Updated By"
+                          sortable>
             {{ props.row.updatedBy }}
           </b-table-column>
-          
-          <b-table-column field="updatedAt" label="Updated At" sortable>
+
+          <b-table-column field="updatedAt"
+                          label="Updated At"
+                          sortable>
             <span class="tag is-info">
               {{ new Date(props.row.updatedAt).toLocaleString() }}
             </span>
           </b-table-column>
-          
-          <b-table-column field="isVisible" label="Visibility">
-            <b-icon v-if="props.row.isVisible" pack="fa" icon="check"></b-icon>
+
+          <b-table-column field="isVisible"
+                          label="Visibility">
+            <b-icon v-if="props.row.isVisible"
+                    pack="fa"
+                    icon="check"></b-icon>
           </b-table-column>
-          
+
           <b-table-column label="Modify">
-            <router-link
-              class="button is-warning"
-              :to="{ name: 'editCustomer', params: { id: props.row.customerAccountId }}"
-            >
-              <b-icon pack="fa" icon="pencil"></b-icon>
+            <router-link class="button is-warning"
+                         :to="{ name: 'editCustomer', params: { id: props.row.customerAccountId }}">
+              <b-icon pack="fa"
+                      icon="pencil"></b-icon>
               <span>Edit</span>
             </router-link>
-            <router-link
-              class="button is-info"
-              :to="{ name: 'manageRates', params: { id: props.row.customerAccountId, name: props.row.accountName }}"
-            >
-              <b-icon pack="fa" icon="pencil"></b-icon>
+            <router-link class="button is-info"
+                         :to="{ name: 'manageRates', params: { id: props.row.customerAccountId, name: props.row.accountName }}">
+              <b-icon pack="fa"
+                      icon="pencil"></b-icon>
               <span>Rates</span>
             </router-link>
-            <button class="button is-info" @click="manageAccountDetails">
-              <b-icon pack="fa" icon="pencil"></b-icon>
+            <button class="button is-info"
+                    @click="manageAccountDetails">
+              <b-icon pack="fa"
+                      icon="pencil"></b-icon>
               <span>Details</span>
             </button>
-            <button class="button is-danger" @click="confirmDelete(props.row.customerAccountId)">
-              <b-icon pack="fa" icon="trash-o"></b-icon>
+            <button class="button is-danger"
+                    @click="confirmDelete(props.row.customerAccountId)">
+              <b-icon pack="fa"
+                      icon="trash-o"></b-icon>
               <span>Delete</span>
             </button>
           </b-table-column>
-          
+
         </template>
 
         <!-- Footer -->
         <template slot="footer">
-            <button class="button is-primary" @click="createCustomerAccount()">
-              Create Account
-            </button>
+          <button class="button is-primary"
+                  @click="createCustomerAccount()">
+            Create Account
+          </button>
         </template>
 
       </b-table>
