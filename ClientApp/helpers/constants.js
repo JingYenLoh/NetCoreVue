b8e8811f9327513825dde6b8bfff475a8c79e8af
Loh Jing Yen <lohjingyen@gmail.com>
2018-01-24 11:59:16 +0800

    Create constants file

---
+++ ClientApp/helpers/constants.js
@@ -0,0 +1,11 @@
+//@ts-check
+
+export const DAYS_OF_WEEK = [
+  { key: 'Sunday', value: 0 },
+  { key: 'Monday', value: 1 },
+  { key: 'Tuesday', value: 2 },
+  { key: 'Wednesday', value: 3 },
+  { key: 'Tursday', value: 4 },
+  { key: 'Friday', value: 5 },
+  { key: 'Saturday', value: 6 }
+]
